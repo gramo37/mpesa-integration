f09a2f425a402e9459fa35eb5ed0bf14e42cb8d5
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2024-09-19 16:39:48 +0530

    tp

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { registerC2BUrl, simulateC2BPayment } from './mpesa';
 
 const app = express();
-const port = 3000;
+const port = 5001;
 
 app.use(bodyParser.json());
 
